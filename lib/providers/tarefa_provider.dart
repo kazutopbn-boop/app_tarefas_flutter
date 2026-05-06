3be2e9268b492c2fde11d6c6c85c2d5591ac039e
kazutopbn-boop <kazutopbn@proton.me>
2026-05-06 20:02:54 -0300

    Atualiza provider e adiciona lógica para buscar tarefa mais próxima do vencimento

--- lib/providers/tarefa_provider.dart
+++ lib/providers/tarefa_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import '../models/tarefa.dart';
 import '../util/db.dart';
 
@@ -9,48 +10,43 @@ class TarefaProvider with ChangeNotifier {
     return [..._tarefas];
   }
 
+  Tarefa? get proximaTarefa {
+    final pendentes = _tarefas.where((t) => !t.realizada).toList();
+
+    if (pendentes.isEmpty) {
+      return null;
+    }
+
+    pendentes.sort((a, b) => a.data.compareTo(b.data));
+    return pendentes.first;
+  }
+
   TarefaProvider() {
     carregarTarefas();
   }
 
   Future<void> carregarTarefas() async {
-    final db = await DB.getDatabase();
-
-    final dados = await db.query('tarefas');
-
-    _tarefas = dados.map((item) {
-      return Tarefa.fromMap(item);
-    }).toList();
-
+    _tarefas = await DB.buscarTarefas();
     notifyListeners();
   }
 
   Future<void> adicionarTarefa(Tarefa tarefa) async {
-    final db = await DB.getDatabase();
-
-    await db.insert('tarefas', tarefa.toMap());
-
+    await DB.inserirTarefa(tarefa);
     await carregarTarefas();
   }
 
   Future<void> editarTarefa(Tarefa tarefa) async {
-    final db = await DB.getDatabase();
-
-    await db.update(
-      'tarefas',
-      tarefa.toMap(),
-      where: 'id = ?',
-      whereArgs: [tarefa.id],
-    );
-
+    await DB.editarTarefa(tarefa);
     await carregarTarefas();
   }
 
   Future<void> excluirTarefa(int id) async {
-    final db = await DB.getDatabase();
+    await DB.excluirTarefa(id);
+    await carregarTarefas();
+  }
 
-    await db.delete('tarefas', where: 'id = ?', whereArgs: [id]);
-
+  Future<void> realizarTarefa(int id) async {
+    await DB.realizarTarefa(id);
     await carregarTarefas();
   }
 }
