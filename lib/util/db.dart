86ca7f4459fa3fa6e6846fa9ad899ee589c4ab91
kazutopbn-boop <kazutopbn@proton.me>
2026-05-06 19:40:23 -0300

    Adiciona categoria ao BD e adiciona uma Tela de Início

--- lib/util/db.dart
+++ lib/util/db.dart
@@ -1,6 +1,8 @@
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
+import '../models/tarefa.dart';
+
 class DB {
   static Database? _database;
 
@@ -14,21 +16,80 @@ class DB {
 
     _database = await openDatabase(
       caminho,
-      version: 1,
+      version: 2,
       onCreate: (db, version) async {
-        await db.execute('''
-          CREATE TABLE tarefas (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            titulo TEXT NOT NULL,
-            descricao TEXT NOT NULL,
-            dataPrevista TEXT NOT NULL,
-            importante INTEGER NOT NULL,
-            realizada INTEGER NOT NULL
-          )
-        ''');
+        await _criarTabela(db);
+      },
+      onUpgrade: (db, oldVersion, newVersion) async {
+        await db.execute('DROP TABLE IF EXISTS tarefas');
+        await _criarTabela(db);
       },
     );
 
     return _database!;
+  }
+
+  static Future<void> _criarTabela(Database db) async {
+    await db.execute('''
+      CREATE TABLE tarefas (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        titulo TEXT NOT NULL,
+        descricao TEXT NOT NULL,
+        dataPrevista TEXT NOT NULL,
+        importante INTEGER NOT NULL,
+        realizada INTEGER NOT NULL,
+        categoria TEXT NOT NULL
+      )
+    ''');
+  }
+
+  static Future<void> inserirTarefa(Tarefa tarefa) async {
+    final db = await getDatabase();
+    await db.insert('tarefas', tarefa.toMap());
+  }
+
+  static Future<List<Tarefa>> buscarTarefas() async {
+    final db = await getDatabase();
+
+    final dados = await db.query(
+      'tarefas',
+      orderBy: 'dataPrevista ASC',
+    );
+
+    return dados.map((item) {
+      return Tarefa.fromMap(item);
+    }).toList();
+  }
+
+  static Future<void> editarTarefa(Tarefa tarefa) async {
+    final db = await getDatabase();
+
+    await db.update(
+      'tarefas',
+      tarefa.toMap(),
+      where: 'id = ?',
+      whereArgs: [tarefa.id],
+    );
+  }
+
+  static Future<void> excluirTarefa(int id) async {
+    final db = await getDatabase();
+
+    await db.delete(
+      'tarefas',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+
+  static Future<void> realizarTarefa(int id) async {
+    final db = await getDatabase();
+
+    await db.update(
+      'tarefas',
+      {'realizada': 1},
+      where: 'id = ?',
+      whereArgs: [id],
+    );
   }
 }
